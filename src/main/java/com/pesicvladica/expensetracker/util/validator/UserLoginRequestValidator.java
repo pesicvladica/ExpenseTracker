4e37642601b425e5d827437ddcf51923462b820d
vladica.pesic <vladica.pesic@intradiem.com>
2025-06-19 08:42:41 +0200

    Login validation if user does not exists
    Change-Id: Ic67c4cf73eee3cff532199cb5ff898fdb3802783

--- src/main/java/com/pesicvladica/expensetracker/util/validator/UserLoginRequestValidator.java
+++ src/main/java/com/pesicvladica/expensetracker/util/validator/UserLoginRequestValidator.java
@@ -2,6 +2,7 @@ package com.pesicvladica.expensetracker.util.validator;
 
 import com.pesicvladica.expensetracker.dto.UserLoginRequest;
 import com.pesicvladica.expensetracker.exception.CredentialsInvalidException;
+import com.pesicvladica.expensetracker.exception.UserAlreadyExistsException;
 import com.pesicvladica.expensetracker.repository.AppUserRepository;
 
 public class UserLoginRequestValidator  implements Validator<UserLoginRequest> {
@@ -26,6 +27,9 @@ public class UserLoginRequestValidator  implements Validator<UserLoginRequest> {
     public void validate(UserLoginRequest obj) {
         if (obj.getUsernameOrEmail().isBlank() || obj.getPassword().isBlank()) {
             throw new CredentialsInvalidException("Username and password cant be blank!");
+        }
+        if (!appUserRepository.existsByUsername(obj.getUsernameOrEmail()) && !appUserRepository.existsByEmail(obj.getUsernameOrEmail())) {
+            throw new CredentialsInvalidException("Username or password invalid!");
         }
         if (obj.getPassword().length() < 8) {
             throw new CredentialsInvalidException("Username or password invalid!");
